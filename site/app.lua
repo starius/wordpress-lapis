6f45c17ba29fe3a9ed05ff4dc7fce2620aa8c48c
Boris Nagaev <bnagaev@gmail.com>
2015-04-06 02:33:41 +0300

    site: enable lapis-console

--- site/app.lua
+++ site/app.lua
@@ -1,8 +1,12 @@
 local lapis = require("lapis")
+local console = require("lapis.console")
+
 local app = lapis.Application()
 
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
 end)
+
+app:match("console", "/console", console.make())
 
 return app
