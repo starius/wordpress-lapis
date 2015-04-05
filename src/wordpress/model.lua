f8b6ca6c8d736848c827b146cb020e42dba7746e
Boris Nagaev <bnagaev@gmail.com>
2015-04-06 02:34:12 +0300

    module wordpress.model, models user, usermeta
    $ cd site
    $ lapis server development
    
    Open http://127.0.0.1:3176/console
    
    Enter the following code:
    
      model = dofile '../src/wordpress/model.lua'
      users = model.Users\select!
      for _, user in ipairs(users) do
        print user.user_nicename
        metas = user\get_usermeta(per_page: 20)\get_page(1)
        for meta in *metas
          print meta.meta_key

---
+++ src/wordpress/model.lua
@@ -0,0 +1,65 @@
+local schema = require("lapis.db.schema")
+local Model = require("lapis.db.model").Model
+local config = require("lapis.config").get()
+
+local model = {}
+
+function model.create_schema()
+    local types = {
+        text = schema.types.varchar,
+        varchar = schema.types.varchar,
+        integer = schema.types.integer,
+        boolean = schema.types.boolean,
+    }
+    if config.postgres then
+        types.id = schema.types.serial({primary_key=true})
+        types.foreign_key = schema.types.foreign_key
+        types.datetime = schema.types.time
+    elseif config.mysql then
+        types.id = schema.types.id
+        types.foreign_key = schema.types.integer
+        types.datetime = schema.types.datetime
+    else
+        error('Unknown database type')
+    end
+
+    schema.create_table("wp_users", {
+        {"ID", types.id},
+        {"user_login", types.varchar(60)},
+        {"user_pass", types.varchar(64)},
+        {"user_nicename", types.varchar(50)},
+        {"user_email", types.varchar(100)},
+        {"user_url", types.varchar(100)},
+        {"user_registered", types.datetime},
+        {"user_activation_key", types.varchar(60)},
+        {"user_status", types.integer},
+        {"display_name", types.varchar(250)},
+    })
+
+    schema.create_table("wp_usermeta", {
+        {"umeta_id", types.id},
+        {"user_id", types.foreign_key},
+        {"meta_key", types.varchar(255)},
+        {"meta_value", types.text},
+    })
+end
+
+model.Users = Model:extend("wp_users", {
+    primary_key = "ID",
+    relations = {
+        {"usermeta", has_many="UserMeta", key="user_id"}
+    },
+})
+
+model.UserMeta = Model:extend("wp_usermeta", {
+    primary_key = "umeta_id",
+    relations = {
+        {"user", belongs_to="Users", key="user_id"}
+    },
+})
+
+-- make `require "models"` return this module
+-- Lapis uses it in function add_relations
+package.loaded['models'] = model
+
+return model
